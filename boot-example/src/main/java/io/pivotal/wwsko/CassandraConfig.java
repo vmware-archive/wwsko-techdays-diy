98500ba91c0fbeb30a3fce9aea593f8e9f546ca0
azwickey-pivotal <azwickey@gopivotal.com>
2015-12-30 21:27:19 -0500

    updates to run in dcos/mesos

--- boot-example/src/main/java/io/pivotal/wwsko/CassandraConfig.java
+++ boot-example/src/main/java/io/pivotal/wwsko/CassandraConfig.java
@@ -1,6 +1,8 @@
 package io.pivotal.wwsko;
 
+import com.datastax.driver.core.Cluster;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cassandra.config.CassandraCqlClusterFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
@@ -19,8 +21,32 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Value("${spring.data.cassandra.keyspace-name}")
     private String _keyspace;
 
+    @Value("${spring.data.cassandra.contact-points}")
+    private String _cp;
+
     public String getKeyspaceName() {
         return _keyspace;
+    }
+
+    @Override
+    @Bean
+    public CassandraCqlClusterFactoryBean cluster() {
+        CassandraCqlClusterFactoryBean bean = new CassandraCqlClusterFactoryBean();
+        bean.setAuthProvider(this.getAuthProvider());
+        bean.setCompressionType(this.getCompressionType());
+        bean.setContactPoints(_cp);
+        bean.setKeyspaceCreations(this.getKeyspaceCreations());
+        bean.setKeyspaceDrops(this.getKeyspaceDrops());
+        bean.setLoadBalancingPolicy(this.getLoadBalancingPolicy());
+        bean.setMetricsEnabled(this.getMetricsEnabled());
+        bean.setPort(this.getPort());
+        bean.setReconnectionPolicy(this.getReconnectionPolicy());
+        bean.setPoolingOptions(this.getPoolingOptions());
+        bean.setRetryPolicy(this.getRetryPolicy());
+        bean.setShutdownScripts(this.getShutdownScripts());
+        bean.setSocketOptions(this.getSocketOptions());
+        bean.setStartupScripts(this.getStartupScripts());
+        return bean;
     }
 
     @Bean
